8ae2c9c0d7de24e0b296874f8142e3bacf9b2c2e
James Halliday <mail@substack.net>
2013-09-26 15:11:45 +0100

    fix for empty strings

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = function (key, opts) {
-    if (opts.lt && !(key < opts.lt)) return false
-    if (opts.lte && !(key <= opts.lte)) return false
-    if (opts.gt && !(key > opts.gt)) return false
-    if (opts.gte && !(key >= opts.gte)) return false
+    if (opts.lt !== undefined && !(key < opts.lt)) return false
+    if (opts.lte !== undefined && !(key <= opts.lte)) return false
+    if (opts.gt !== undefined && !(key > opts.gt)) return false
+    if (opts.gte !== undefined && !(key >= opts.gte)) return false
     
-    if (opts.start && key < opts.start) return false
-    if (opts.end && key > opts.end) return false
-    if (opts.min && key < opts.min) return false
-    if (opts.max && key > opts.max) return false
+    if (opts.start !== undefined && key < opts.start) return false
+    if (opts.end !== undefined && key > opts.end) return false
+    if (opts.min !== undefined && key < opts.min) return false
+    if (opts.max !== undefined && key > opts.max) return false
     
     return true
 };
